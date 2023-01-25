157bdbdda08beb21947e246c8039ba4778778023
Valdonix2 <123013224+Valdonix2@users.noreply.github.com>
2023-01-25 18:37:26 +0200

    Update spawnwhat.lua

--- spawnwhat.lua
+++ spawnwhat.lua
@@ -1,6 +1,14 @@
+local whatsound = Instance.new("Sound", game.Workspace)
+whatsound.SoundId = "rbxassetid://9114352428"
+
+if not whatsound.IsLoaded then
+	whatsound.Loaded:Wait()
+end
+
 game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
 local what = (math.random(1, 8))
 print (what)
 if what == 1 then
+	whatsound:Play()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/Valdonix2/gaming/main/what.lua'))()
 end
